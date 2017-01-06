66e0c58e8059c0e338595be07fe8f7d781b0ba42
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2017-01-06 12:12:44 +0100

    Created project for scrapping currency conversion rates from websites

---
+++ Currency conversion sources.md
@@ -0,0 +1,7 @@
+
+Website - [http://www.xe.com/](http://www.xe.com/)
+
+Converter - [http://www.xe.com/currencytables/?from=USD&date=2016-10-08](http://www.xe.com/currencytables/?from=USD&date=2016-10-08)
+
+History data - [http://www.ukforex.co.uk/forex-tools/historical-rate-tools/historical-exchange-rates](http://www.ukforex.co.uk/forex-tools/historical-rate-tools/historical-exchange-rates)
+
---
+++ CurrencyWebScrapper/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+
+namespace CurrencyWebScrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScrapingBrowser Browser = new ScrapingBrowser();
+            Browser.AllowAutoRedirect = true; // Browser has settings you can access in setup
+            Browser.AllowMetaRedirect = true;
+
+            WebPage PageResult = Browser.NavigateToPage(new Uri("http://www.x-rates.com/historical/?from=USD&amount=1&date=2017-01-06"));
+            //HtmlNode TitleNode = PageResult.Html.CssSelect(".navbar-brand").FirstOrDefault();
+            //string PageTitle = TitleNode.InnerText;
+
+            List<string> Names = new List<string>();
+            var Table = PageResult.Html.CssSelect(".ratesTable");
+
+            var myTable = Table.ElementAt(1);
+
+            foreach (var row in myTable.SelectNodes("tbody/tr"))
+            {
+                foreach (var cell in row.SelectNodes("td"))
+                {
+                    Console.WriteLine(cell.InnerText);
+                }
+            }
+        }
+
+        private static void GetPageUsingStandardWebRequest()
+        {
+            string url = "http://www.x-rates.com/historical/?from=USD&amount=1&date=2017-01-06";
+            string strResult = "";
+            WebResponse objResponse;
+            WebRequest objRequest = System.Net.HttpWebRequest.Create(url);
+
+            objResponse = objRequest.GetResponse();
+
+            using (StreamReader sr = new StreamReader(objResponse.GetResponseStream()))
+            {
+                strResult = sr.ReadToEnd();
+                // Close and clean up the StreamReader
+                sr.Close();
+            }
+
+            // Display results to a webpage
+            Console.Write(strResult);
+        }
+    }
+}
